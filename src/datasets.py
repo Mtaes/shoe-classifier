748ef0dc69adda201f42d53179db6c9a3bea43c3
Mtaes <Mtaes@users.noreply.github.com>
2022-05-31 11:22:27 +0200

    Add data transforms and refactor code

--- src/datasets.py
+++ src/datasets.py
@@ -5,27 +5,38 @@ import torch
 from PIL import Image
 from torch.utils.data import Dataset
 from torchvision.datasets import ImageFolder
+from torchvision.transforms import RandomRotation
 
 
 class FashionMNISTDataset(Dataset):
-    def __init__(self, data_path: Path):
+    def __init__(self, data_path: Path, train: bool = False):
         with open(data_path / "images.npy", mode="rb") as f:
             self.images = np.load(f)
         with open(data_path / "tlabels.npy", mode="rb") as f:
             self.labels = np.load(f)
+        if train:
+            self.transforms = RandomRotation(15)
+        else:
+            self.transforms = None
 
     def __getitem__(self, idx):
         image = (
             torch.tensor(self.images[idx].reshape(1, 28, 28), dtype=torch.float32) / 255
         )
         target = torch.tensor(self.labels[idx], dtype=torch.float32)
+        if self.transforms is not None:
+            image = self.transforms(image)
         return image, target
 
     def __len__(self):
         return len(self.labels)
 
 
-def get_shoe_dataset(root: str):
+def get_shoe_dataset(root: str, train: bool = False):
+    if train:
+        transforms = RandomRotation((-10, 10))
+    else:
+        transforms = None
     return ImageFolder(
         root,
         loader=lambda path: torch.from_numpy(
@@ -33,4 +44,5 @@ def get_shoe_dataset(root: str):
         ).reshape((1, 28, 28))
         / 255,
         target_transform=lambda t: torch.tensor(t, dtype=torch.float32),
+        transform=transforms,
     )
--- src/train_model_1.py
+++ src/train_model_1.py
@@ -1,58 +1,25 @@
 from pathlib import Path
 
-import torch
-from pytorch_lightning import Trainer, seed_everything
-from pytorch_lightning.callbacks import ModelCheckpoint
-from pytorch_lightning.loggers import TensorBoardLogger
-from torch.utils.data import DataLoader
+from pytorch_lightning import seed_everything
 
 from datasets import FashionMNISTDataset
 from models import ShoeClassifier
+from utils import get_args, get_dataloader, get_trainer
 
 if __name__ == "__main__":
     seed_everything(seed=42, workers=True)
+    args = get_args()
     data_path = Path("..") / "data" / "fashion-mnist-prep"
-    train_dataset = FashionMNISTDataset(data_path / "train")
+    train_dataset = FashionMNISTDataset(data_path / "train", train=True)
     val_dataset = FashionMNISTDataset(data_path / "val")
     test_dataset = FashionMNISTDataset(data_path / "test")
 
-    batch = 4096
-    workers = 4
-    train_loader = DataLoader(
-        train_dataset,
-        batch_size=batch,
-        num_workers=workers,
-        pin_memory=True,
-    )
-
-    val_loader = DataLoader(
-        val_dataset,
-        batch_size=batch,
-        num_workers=workers,
-        pin_memory=True,
-        shuffle=False,
-    )
-
-    test_loader = DataLoader(
-        test_dataset,
-        batch_size=batch,
-        num_workers=workers,
-        pin_memory=True,
-        shuffle=False,
-    )
+    train_loader = get_dataloader(train_dataset, args, shuffle=True)
+    val_loader = get_dataloader(val_dataset, args)
+    test_loader = get_dataloader(test_dataset, args)
 
     log_path = Path("..") / "logs"
-    model = ShoeClassifier()
-    trainer = Trainer(
-        default_root_dir=str(log_path),
-        gpus=1 if torch.cuda.is_available() else None,
-        max_epochs=200,
-        logger=TensorBoardLogger(log_path, name="classifier_1"),
-        log_every_n_steps=1,
-        callbacks=[ModelCheckpoint(monitor="epoch_val/loss")],
-        deterministic=True,
-        detect_anomaly=True,
-    )
-
+    model = ShoeClassifier(lr=args.lr)
+    trainer = get_trainer(args.epochs, log_path, name="classifier_1")
     trainer.fit(model, train_loader, val_loader)
     trainer.test(dataloaders=test_loader, ckpt_path="best")
